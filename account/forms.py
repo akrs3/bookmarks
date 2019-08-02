c5fb08a3743fe10a3ddd71c425055d6013a2bebd
Anne Kelly <ctinfoapple2@iMac-de-Anne.local>
2019-08-02 10:25:33 -0300

    Expandindo o modelo de usuarios e permitindo edicoes e trocas de senhas

--- account/forms.py
+++ account/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
-
+from .models import Profile
 
 class LoginForm(forms.Form):
     username = forms.CharField()
@@ -22,3 +22,17 @@ class UserRegistrationForm(forms.ModelForm):
         if cd['password'] != cd['password2']:
             raise forms.ValidationError('Passwords don\'t match.')
         return cd['password2']
+
+
+# permite que usuarios editem seus atributos basicos de user do Django
+class UserEditForm(forms.ModelForm):
+    class Meta:
+        model = User
+        fields = ('first_name', 'last_name', 'email')
+
+
+# permite que usuarios editem os atributos deles modelados pela classe Profile
+class ProfileEditForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ('date_of_birth', 'photo')
